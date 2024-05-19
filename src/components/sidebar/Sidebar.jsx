a0b594b9f9507928b8f8a3feba5ac491600b093f
Ahsaan Ullah <ahsaanu70@gmail.com>
2024-05-19 10:12:32 +0500

    sidebar functionaliy done

--- src/components/sidebar/Sidebar.jsx
+++ src/components/sidebar/Sidebar.jsx
@@ -1,40 +1,43 @@
-import React from 'react'
-import {assets} from '../../assets/assets'
-import './Sidebar.css'
+import React from "react";
+import { assets } from "../../assets/assets";
+import "./Sidebar.css";
 
 const Sidebar = () => {
+  const [extended, setExtended] = React.useState(false);
   return (
-    <div className='sidebar'>
+    <div className="sidebar">
       <div className="top">
-        <img className='menu' src={assets.menu_icon} alt="" />
+        <img onClick={()=> setExtended(prev=>!prev)} className="menu" src={assets.menu_icon} alt="" />
         <div className="new_chat">
           <img src={assets.plus_icon} alt="" />
-          <p>New Chat</p>
+          {extended ? <p>New Chat</p> : null}
         </div>
-        <div className="recent">
-          <p className="recent-title"> Recent</p>
-          <div className="recent-entry">
-            <img src={assets.menu_icon} alt="" />
-            <p>What is react...</p>
+        {extended ? (
+          <div className="recent">
+            <p className="recent-title"> Recent</p>
+            <div className="recent-entry">
+              <img src={assets.menu_icon} alt="" />
+              <p>What is react...</p>
+            </div>
           </div>
-        </div>
+        ) : null}
       </div>
       <div className="bottom">
         <div className="bottom-item recent-entry">
           <img src={assets.question_icon} alt="" />
-          <p>Help</p>
+          {extended?<p>Help</p>:null}
         </div>
         <div className="bottom-item recent-entry">
           <img src={assets.history_icon} alt="" />
-          <p>Activity</p>
+          {extended?<p>Activity</p>:null}
         </div>
         <div className="bottom-item recent-entry">
           <img src={assets.setting_icon} alt="" />
-          <p>Settings</p>
+          {extended?<p>Settings</p>:null}
         </div>
       </div>
     </div>
-  )
-}
+  );
+};
 
-export default Sidebar
+export default Sidebar;
