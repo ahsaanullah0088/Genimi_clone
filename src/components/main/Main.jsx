ebfc3ba4718c4d3d1fc4f3f48f2c30902514ff82
Ahsaan Ullah <ahsaanu70@gmail.com>
2024-05-21 12:48:28 +0500

    make the preloader animation

--- src/components/main/Main.jsx
+++ src/components/main/Main.jsx
@@ -15,51 +15,52 @@ const Main = () => {
           <img src={assets.user_icon} alt="" />
         </div>
         <div className="main-component">
-          {! showResult ? 
-          <div>
-                    <div className="great">
-            <p>
-              <span>Hello, Ahsaan</span>
-            </p>
-            <p>How can i help you today?</p>
-          </div>
-          <div className="cards">
-            <div className="card">
-              <p>Suggest beautiful plae o see on an upcomming road trip</p>
-              <img src={assets.compass_icon} alt="" />
-            </div>
-            <div className="card">
-              <p>Brifely summarize this concept: urban planning</p>
-              <img src={assets.bulb_icon} alt="" />
-            </div>
-            <div className="card">
-              <p>Brainstrom team bonding activits for our work retreat</p>
-              <img src={assets.message_icon} alt="" />
-            </div>
-            <div className="card">
-              <p>Improve the readability of the following code</p>
-              <img src={assets.code_icon} alt="" />
-            </div>
-          </div>
-          </>
-          : <div className="result">
-            <div className="result_title">
-              <img src={assets.user_icon} alt="" />
-              <p>{recentPrompt}</p>
-            </div>
-            <div className="result-data">
-              <img src={assets.gemini_icon} alt="" />
-              {loading
-              ? <div className="loader">
-                <hr />
-                <hr />
-                <hr />
+          {!showResult ? 
+            <div>
+              <div className="great">
+                <p>
+                  <span>Hello, Ahsaan</span>
+                </p>
+                <p>How can i help you today?</p>
               </div>
-              :
-              <p dangerouslySetInnerHTML={{__html:resultData}}></p>}
-            
-          </div>
-          </div>
+              <div className="cards">
+                <div className="card">
+                  <p>Suggest beautiful place to see on an upcoming road trip</p>
+                  <img src={assets.compass_icon} alt="" />
+                </div>
+                <div className="card">
+                  <p>Briefly summarize this concept: urban planning</p>
+                  <img src={assets.bulb_icon} alt="" />
+                </div>
+                <div className="card">
+                  <p>Brainstorm team bonding activities for our work retreat</p>
+                  <img src={assets.message_icon} alt="" />
+                </div>
+                <div className="card">
+                  <p>Improve the readability of the following code</p>
+                  <img src={assets.code_icon} alt="" />
+                </div>
+              </div>
+            </div>
+          :
+            <div className="result">
+              <div className="result_title">
+                <img src={assets.user_icon} alt="" />
+                <p>{recentPrompt}</p>
+              </div>
+              <div className="result-data">
+                <img src={assets.gemini_icon} alt="" />
+                {loading ?
+                  <div className="loader">
+                    <hr />
+                    <hr />
+                    <hr />
+                  </div>
+                :
+                  <p dangerouslySetInnerHTML={{__html:resultData}}></p>
+                }
+              </div>
+            </div>
           }
 
           <div className="main-bottom">
@@ -72,7 +73,7 @@ const Main = () => {
               </div>
             </div>
             <p className="bottom-info">
-              genimi may display inacurate info, includeing about people, so double check its responses. Your privacy and Genimi App
+              Genimi may display inaccurate info, including about people, so double check its responses. Your privacy and Genimi App
             </p>
           </div>
         </div>
